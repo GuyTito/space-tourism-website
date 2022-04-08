7f5f5073062699c93b9665f70ad0a4482ddc9261
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-08 12:20:58 +0000

    added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 
 </script>
 
 <template>
-  <div class="bg-space-mb bg-no-repeat bg-center">
-    <header>
-      
-    </header>
-  
-    <RouterView />
-  </div>
+  <Navbar />
+
+  <RouterView />
 </template>
 
 
