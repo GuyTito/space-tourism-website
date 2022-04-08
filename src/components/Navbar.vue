d4ab5b252dd1b92db06057a3312ae21fc3d3fdc1
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-08 12:20:38 +0000

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, } from 'vue-router'
+
+
+</script>
+
+
+<template>
+  <header class="mx-6 flex justify-between items-center mt-6">
+    <RouterLink to="/">
+      <img src="assets/shared/logo.svg" alt="logo">
+    </RouterLink>
+
+    <button>
+      <img src="assets/shared/icon-hamburger.svg" alt="menu">
+    </button>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
