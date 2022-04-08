f33a91a151bc149b8a1ddfff896bac4a68d1a600
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-08 15:04:04 +0000

    modified explore button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,12 +6,12 @@
   <div class="text-center mx-6 space-y-4 mt-12">
     <span class="text-space-blue font-bc text-[16px] tracking-[2.7px]">SO, YOU WANT TO TRAVEL TO</span>
 
-    <h1>SPACE</h1>
+    <h1 class="leading-[150px]">SPACE</h1>
 
     <p class="text-space-blue text-[15px] leading-[25px]">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
   </div>
 
-  <div class="mt-20 pb-12 text-center">
-    <button class="bg-white text-black font-serif text-xl rounded-full h-[150px] w-[150px] ">EXPLORE</button>
+  <div class="mt-20 mb-5 text-center h-[246px] max-w-[246px] mx-auto rounded-full grid place-items-center mix-blend-normal hover:bg-white/10 pointer-events-none">
+    <button class="bg-white text-space-black font-serif text-xl rounded-full h-[150px] w-[150px] pointer-events-auto">EXPLORE</button>
   </div>
 </template>
