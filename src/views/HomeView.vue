78c56cc93b973e9d7c40d10ff47273be226b7395
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-08 11:01:00 +0000

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-  <div class="text-center px-6">
+  <div class="text-center mx-6 space-y-4 mt-12">
     <span class="text-space-blue font-bc text-[16px] tracking-[2.7px]">SO, YOU WANT TO TRAVEL TO</span>
+
     <h1>SPACE</h1>
+
     <p class="text-space-blue text-[15px] leading-[25px]">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+  </div>
+
+  <div class="mt-20 pb-12 text-center">
+    <button class="bg-white text-black font-serif text-xl rounded-full h-[150px] w-[150px] ">EXPLORE</button>
   </div>
 </template>
