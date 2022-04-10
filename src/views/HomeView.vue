fabf32ae762e183668a82ecb9168ced18a55b01a
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-10 11:03:43 +0000

    transition to the "Explore" button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
     <p class="text-space-blue text-[15px] leading-[25px]">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
   </div>
 
-  <div class="mt-20 mb-5 text-center h-[246px] max-w-[246px] mx-auto rounded-full grid place-items-center mix-blend-normal hover:bg-white/10 pointer-events-none">
+  <div class="mt-20 mb-5 text-center h-[246px] max-w-[246px] mx-auto rounded-full grid place-items-center mix-blend-normal pointer-events-none hover:bg-white/10 transition-all duration-300">
     <button class="bg-white text-space-black font-serif text-xl rounded-full h-[150px] w-[150px] pointer-events-auto">EXPLORE</button>
   </div>
 </template>
