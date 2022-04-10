354c8712a0aafccbc005ffdc7bc70cce089e663c
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-10 16:39:20 +0000

    tablet view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,15 @@
 </script>
 
 <template>
-  <div class="text-center mx-6 space-y-4 mt-12">
-    <span class="text-space-blue font-bc text-[16px] tracking-[2.7px]">SO, YOU WANT TO TRAVEL TO</span>
+  <div class="text-center mx-6 md:mx-40 space-y-4 md:space-y-6 mt-12 md:mt-[106px]">
+    <span class="text-space-blue font-bc text-[16px] md:text-[20px] tracking-[2.7px] md:tracking-[3.4px] leading-6">SO, YOU WANT TO TRAVEL TO</span>
 
     <h1 class="leading-[150px]">SPACE</h1>
 
-    <p class="text-space-blue text-[15px] leading-[25px]">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+    <p class="text-space-blue text-[16px] leading-6 md:leading-7">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
   </div>
 
-  <div class="mt-20 mb-5 text-center h-[246px] max-w-[246px] mx-auto rounded-full grid place-items-center mix-blend-normal pointer-events-none hover:bg-white/10 transition-all duration-300">
-    <button class="bg-white text-space-black font-serif text-xl rounded-full h-[150px] w-[150px] pointer-events-auto">EXPLORE</button>
+  <div class="mt-20 md:mt-[156px] mb-5 md:mb-12 text-center h-[246px] max-w-[246px] md:h-[397px] md:max-w-[397px] mx-auto rounded-full grid place-items-center mix-blend-normal pointer-events-none hover:bg-white/10 transition-all duration-300">
+    <button class="bg-white text-space-black font-serif text-xl md:text-[32px] rounded-full h-[150px] w-[150px] md:h-[242px] md:w-[242px] pointer-events-auto">EXPLORE</button>
   </div>
 </template>
