6f17796ab86bc4702ea0f0906fc38b71f14544ca
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-11 14:44:10 +0000

    Create useBackgroundImage.js

---
+++ src/composables/useBackgroundImage.js
@@ -0,0 +1,9 @@
+const useBackgrounImage = (current_route, given_route, bg_images) => {
+  if ( current_route == given_route ) {
+    document.body.className = `${bg_images} bg-no-repeat bg-cover`
+    return
+  }
+  document.body.className = 'bg-home-mob md:bg-home-tab lg:bg-home-desk bg-no-repeat bg-cover'
+}
+
+export default useBackgrounImage
