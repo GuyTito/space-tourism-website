dac47042afe906f1013f473b88f08c22efc4ab8f
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-11 15:59:10 +0000

    center explore button text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { RouterLink, useRouter, } from 'vue-router'
+import { useRouter, } from 'vue-router'
 import useBackgroundImage from '@/composables/useBackgroundImage.js'
 
 const body_classes = 'bg-home-mob lg:bg-home-desk'
@@ -20,7 +20,9 @@ useBackgroundImage(current_route, 'home', body_classes)
     </div>
   
     <div class="mt-20 md:mt-[156px] lg:mt-0 mb-5 md:mb-12 lg:mb-0 text-center h-[246px] max-w-[246px] md:h-[397px] md:max-w-[397px] lg:h-[450px] lg:max-w-[450px] lg:w-[450px] mx-auto lg:mx-0 rounded-full grid place-items-center mix-blend-normal pointer-events-none hover:bg-white/10 transition-all duration-300">      
-      <RouterLink to="/destination" class="bg-white text-space-black font-serif text-xl md:text-[32px] rounded-full h-[150px] w-[150px] md:h-[242px] md:w-[242px] lg:h-[274px] lg:w-[274px]  pointer-events-auto">EXPLORE</RouterLink>
+      <router-link to="/destination" class="bg-white text-space-black font-serif text-xl md:text-[32px] rounded-full h-[150px] w-[150px] md:h-[242px] md:w-[242px] lg:h-[274px] lg:w-[274px] pointer-events-auto grid place-items-center">
+        EXPLORE
+      </router-link>
     </div>
   </div>
 </template>
