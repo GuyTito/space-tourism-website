9a76da7a36fdd4f9857645411e25a21385baa37d
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-13 12:15:19 +0000

    added footer

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,11 @@ import Navbar from './components/Navbar.vue';
   <Navbar />
 
   <RouterView />
+
+  <footer class="mt-10 mb-2 text-center text-[10px] text-white/60">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" class="underline hover:font-bold">Frontend Mentor</a>. 
+      Coded by <a href="https://github.com/GuyTito/space-tourism-website"  target="_blank" class="underline hover:font-bold">GuyTito</a>.
+  </footer>
 </template>
 
 
