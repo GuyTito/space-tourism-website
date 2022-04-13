111fe5dfb877cd9991e083e4bcc905406e47d795
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-13 09:55:51 +0000

    refactor html and classes😁

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,9 +12,10 @@ useBackgroundImage(current_route, 'home', body_classes)
 <template>
   <div class="lg:mt-[150px] lg:flex justify-between lg:mx-[165px] lg:mb-10">
     <div class="text-center lg:text-left mx-6 md:mx-40 lg:mx-0 space-y-4 md:space-y-6 mt-12 md:mt-[106px] lg:mt-0 lg:max-w-[450px]">
-      <span class="text-space-blue above-h1">SO, YOU WANT TO TRAVEL TO</span>
-  
-      <h1 class="leading-[150px] lg:leading-[172px]">SPACE</h1>
+      <h1 class="flex flex-col">
+        <span class="text-space-blue page-h">SO, YOU WANT TO TRAVEL TO</span>
+        <span class="h1 leading-[150px] lg:leading-[172px]">SPACE</span>
+      </h1>
   
       <p class="text-space-blue text-[16px] lg:text-lg leading-6 md:leading-7">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
     </div>
