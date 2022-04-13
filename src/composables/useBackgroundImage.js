bd4689d1369ab1be16c1d90c337e3ffb2733bfa5
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-13 10:03:49 +0000

    added backup bg color to mask the white flash when pages are navigated

--- src/composables/useBackgroundImage.js
+++ src/composables/useBackgroundImage.js
@@ -1,6 +1,6 @@
 const useBackgrounImage = (current_route, given_route, bg_images) => {
   if ( current_route == given_route ) {
-    document.body.className = `${bg_images} bg-no-repeat bg-cover`
+    document.body.className = `bg-space-black ${bg_images} bg-no-repeat bg-cover`
     return
   }
   document.body.className = 'bg-home-mob md:bg-home-tab lg:bg-home-desk bg-no-repeat bg-cover'
